7ab3d1402a16516dff71a9d5a3c95262b0d71c15
ShubhamNakashe <114340052+ShubhamNakashe@users.noreply.github.com>
2023-10-12 10:10:44 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,16 +1,26 @@
-package com.example;
-
+package example;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
 public class App {
-
-    @Test
-    public void testGoogleSearch() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Desktop\\Jenkins\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-        driver.quit();
-    }
+private WebDriver driver;
+@Test
+public void testEasy() {
+driver.get("http://demo.guru99.com/test/guru99home/");
+String title = driver.getTitle();
+Assert.assertTrue(title.contains("Demo Guru99 Page"));
+}
+@BeforeTest
+public void beforeTest() {
+System.setProperty("webdriver.chrome.driver",
+"C:\\Users\\Admin\\Desktop\\Jenkins\\chromedriver.exe");
+driver = new ChromeDriver();
+}
+@AfterTest
+public void afterTest() {
+driver.quit();
+}
 }
